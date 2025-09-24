84570c75883324ca8dbae10a821bbf0f283b4f2d
abdulfaiq47 <abdulfaiq47@gmail.com>
2025-09-24 11:28:26 +0500

    Fix Build

--- app/page.js
+++ app/page.js
@@ -2,13 +2,15 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Login from "./login/page";
 import { toast, ToastContainer } from 'react-toastify'
+import Link from "next/link";
 
 
 export default function Home() {
   return (
     <main>
+      <Link href='/login' ><button  >Login</button></Link>
 
-      <Login />
+      
     </main>
 
   );
