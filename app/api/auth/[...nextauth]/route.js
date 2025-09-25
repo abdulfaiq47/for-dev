668b7d2877a3ba9298d4016aa7fa2016969aede8
abdulfaiq47 <abdulfaiq47@gmail.com>
2025-09-25 08:28:43 +0500

    Updated

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -38,7 +38,7 @@ const authOptions = {
     session: {
         strategy: "jwt"
     },
-    secret: process.env.AUTH_SECRET,
+    secret: process.env.NEXTAUTH_SECRET,
     pages: {
         signIn: "/login",
     }
